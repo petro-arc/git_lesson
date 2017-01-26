20dac0dfdf0eeb3c4650f16a29376a15da58d15f
petro-arc <chucksmith@petro-arc.com>
2017-01-26 16:18:07 -0700

    changed x to z and multiply instead of square new value 12

--- src/my_square.py
+++ src/my_square.py
@@ -1,8 +1,9 @@
 """ 
 here is some documentation x
+changed x to z and multiply instead of square new value 12
 """
 
-def my_square(x):
-	return(x ** 2)
+def my_square(z):
+	return(z * z)
 
-print(my_square(5))
+print(my_square(12))
